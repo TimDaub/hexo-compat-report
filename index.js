279efbe102acf8a79b4d3ec1b4329e54e12e4b4a
Tim Daubenschütz <tim@daubenschuetz.de>
2020-05-06 17:11:09 +0200

    Prepare dependencies

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
 // @format
-const ejs = require("ejs");
 const fs = require("hexo-fs");
 const render = require("./kumascript/src/render");
 const Templates = require("./kumascript/src/templates");
 const config = require("./kumascript/src/config");
+const sass = require("node-sass");
+const path = require("path");
+const inliner = require("sass-inline-svg");
 
-// Resources:
-// - https://github.com/hexojs/hexo/issues/2874#issuecomment-355675749
-// - https://github.com/m80126colin/hexo-tag-owl/blob/master/lib/index.js#L26
-// - https://hexo.io/api/tag#Async-Rendering
+const paths = {
+  styles: [
+    `${__dirname}/kuma/kuma/static/styles/wiki-compat-tables.scss`,
+    `${__dirname}/stumptown-renderer/client/src/document/ingredients/browser-compatibility-table/bcd.scss`
+  ],
+  icons: `${__dirname}/stumptown-renderer/client/src/document/ingredients/browser-compatibility-table/`
+};
+
 hexo.extend.tag.register(
   "compat",
   async function(args) {
     const tag = args[0];
     const s = `{{Compat("${tag}")}}`;
     const templates = new Templates(config.macrosDirectory);
-    let [res, err] = await render(s, templates, {});
-    if (err) throw err;
-    return res;
+    let [table, err] = await render(s, templates, {
+      $0: tag,
+      locale: "en-US"
+    });
+    // NOTE: For some reason, err is an array.
+    if (err.length) throw new Error("Compatibility table rendering failed");
+    const style = sass.renderSync({
+      file: path.normalize(paths.styles[0]),
+      outputStyle: "compressed"
+    });
+
+    const style2 = sass.renderSync({
+      file: path.normalize(paths.styles[1]),
+      outputStyle: "compressed",
+      functions: {
+        // NOTE: We convert all svgs to inline svgs
+        url: inliner(path.normalize(paths.icons))
+      }
+    });
+    const doc = `
+			<!doctype html>
+				<head>
+          <base href="https://developer.mozilla.org">
+          <style>body { background-color: "white"; font-family: x-locale-heading-primary,zillaslab,Palatino,"Palatino Linotype",x-locale-heading-secondary,serif }</style>
+					<style>${style.css.toString()}</style>
+					<style>${style2.css.toString()}</style>
+				</head>
+				<body>
+					${table}
+				</body>
+			</html>`;
+    return `
+      <iframe src="about:blank">
+        <body>
+          <p>hello</p>
+        </body>
+      </iframe>
+    `;
   },
   { async: true }
 );
